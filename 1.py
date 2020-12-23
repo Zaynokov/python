c9699fbbab8c61d6d860a94bdba8d16647c6652c
Zaynokov <vlerii99@mail.ru>
2020-12-23 22:08:52 +0300

    Complete

---
+++ 1.py
@@ -0,0 +1,16 @@
+number_1 = 3
+number_2 = 11
+number_3 = 5
+number_4 = 8
+
+print(number_1)
+print(number_2)
+print(number_3)
+print(number_4)
+
+a = input("Введите ваше любимое число: ")
+b = input("Введите ваш возраст: ")
+c = input("Введите ваше имя: ")
+d = input("Введите ваше любимое блюдо: ")
+
+print(f"Ваше любимое число: {a}\nВаш возраст: {b}\nВаше имя: {c}\nВаше любимое блюдо: {d}")
